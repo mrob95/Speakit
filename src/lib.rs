0e9a16bd120c4162f19100083595cfaa7a6cf4c6
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2020-02-22 13:30:03 +0000

    Use text forms for words

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,54 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+const SPOKEN_NUMBERS: [&'static str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
 fn _split_symbol(symbol: &str, numbers: bool, max_len: usize) -> String {
     // let mut words = String::new();
     let mut words = String::with_capacity(symbol.len() * 2);
     let mut prev_numeric = false;
     let mut prev_space = true;
     let mut num_spaces = 1;
+    // If the last character wasn't a space, add one
+    // if we have reached the target number of words, break out of the
     macro_rules! add_space {
         () => {
-            if num_spaces == max_len { break; }
-            words.push_str(" ");
-            num_spaces += 1;
-            prev_space = true;
+            if !prev_space {
+                if num_spaces == max_len { break; }
+                words.push_str(" ");
+                num_spaces += 1;
+                prev_space = true;
+            }
         };
     }
     for c in symbol.chars() {
         if !c.is_alphanumeric() {
             // Character is not alphanumeric, assume that it is a separator character
-            if !prev_space {
-                add_space!();
-            }
+            add_space!();
             prev_numeric = false;
             continue;
         }
 
         if c.is_numeric() {
-            if !prev_numeric && !prev_space && numbers {
-                // transition from alphabetic to numeric
+            if numbers {
                 add_space!();
+                let i = c.to_digit(10).unwrap();
+                words.push_str(SPOKEN_NUMBERS[i as usize]);
+                prev_space = false;
             }
             prev_numeric = true;
-        } else if (prev_numeric || c.is_uppercase()) && !prev_space {
-            // Transition from numeric to alphabetic, or
-            // uppercase character, insert space if necessary
-            add_space!();
-            prev_numeric = false;
         } else {
-            prev_numeric = false;
-        }
-
-        if !(prev_numeric && !numbers) {
-            // If we aren't including numbers and the character is a number,
-            // then prev_numeric was set above and we don't include the character.
+            if prev_numeric || c.is_uppercase() {
+                // Transition from numeric to alphabetic, or
+                // uppercase character, insert space if necessary
+                add_space!();
+            }
             words.push(c);
             prev_space = false;
+            prev_numeric = false;
         }
     }
+
     if prev_space {
         // Remove trailing space if present
         words.pop();
@@ -106,18 +108,18 @@ fn test_separators() {
 
 #[test]
 fn test_numbers() {
-    assert_eq!(_split_symbol("999", true, 0), "999");
+    assert_eq!(_split_symbol("999", true, 0), "nine nine nine");
     assert_eq!(
         _split_symbol("99module_aTestCase", true, 0),
-        "99 module a Test Case"
+        "nine nine module a Test Case"
     );
     assert_eq!(
         _split_symbol("0_A1B2C3DEF99", true, 0),
-        "0 A 1 B 2 C 3 D E F 99"
+        "zero A one B two C three D E F nine nine"
     );
-    assert_eq!(_split_symbol("_9a", true, 0), "9 a");
-    assert_eq!(_split_symbol("_a9", true, 0), "a 9");
-    assert_eq!(_split_symbol("9aa99", true, 0), "9 aa 99");
+    assert_eq!(_split_symbol("_9a", true, 0), "nine a");
+    assert_eq!(_split_symbol("_a9", true, 0), "a nine");
+    assert_eq!(_split_symbol("9aa99", true, 0), "nine aa nine nine");
 }
 
 #[test]
@@ -135,13 +137,13 @@ fn test_no_numbers() {
 
 #[test]
 fn test_max_len() {
-    assert_eq!(_split_symbol("99module_aTestCase", true, 3), "99 module a");
+    assert_eq!(_split_symbol("99module_aTestCase", true, 3), "nine nine module");
     assert_eq!(
         _split_symbol("99module_aTestCase", false, 3),
         "module a Test"
     );
     assert_eq!(_split_symbol("0_A1B2C3DEF99", false, 3), "A B C");
-    assert_eq!(_split_symbol("0_A1B2C3DEF99", true, 3), "0 A 1");
+    assert_eq!(_split_symbol("0_A1B2C3DEF99", true, 3), "zero A one");
 }
 
 #[test]
@@ -160,12 +162,12 @@ fn test_list() {
             0
         ),
         vec![
-            "999",
-            "99 module a Test Case",
-            "0 A 1 B 2 C 3 D E F 99",
-            "9 a",
-            "a 9",
-            "9 aa 99"
+            "nine nine nine",
+            "nine nine module a Test Case",
+            "zero A one B two C three D E F nine nine",
+            "nine a",
+            "a nine",
+            "nine aa nine nine"
         ]
     );
 }
